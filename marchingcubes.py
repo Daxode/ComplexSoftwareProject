49378511d004f8f75b5675a689b82cbf71ed7bb0
dani485b <dani485b@gmail.com>
2020-09-20 18:56:31 +0200

    testing noise and mouse implementation

--- marchingcubes.py
+++ marchingcubes.py
@@ -1,6 +1,6 @@
 from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import Fog
+from panda3d.core import Fog, loadPrcFile
 from panda3d.core import TextNode
 from direct.gui.OnscreenText import OnscreenText
 from direct.showbase.DirectObject import DirectObject
@@ -29,7 +29,7 @@ from panda3d.core import NodePath
 from panda3d.core import Thread
 from panda3d.core import TransparencyAttrib
 from panda3d.core import Shader
-from panda3d.core import Vec3F, PTAVecBase3f
+from panda3d.core import Vec3F, PTAVecBase3f, PTAVecBase2f
 from panda3d.core import ShaderAttrib, ComputeNode
 from panda3d.core import GeomEnums
 from panda3d.core import CullBinManager
@@ -42,7 +42,7 @@ from typing import Tuple, List
 from panda3d.core import ShaderBuffer, FrameBufferProperties, GraphicsPipe, GraphicsOutput
 from panda3d.core import GraphicsBuffer
 from panda3d.core import BamWriter
-
+loadPrcFile("config/Config.prc")
 class FogDemo(ShowBase):
 
     def __init__(self):
@@ -50,7 +50,6 @@ class FogDemo(ShowBase):
         # create a window and set up everything we need for rendering into it.
         ShowBase.__init__(self)
         self.base = base
-        self.base.setFrameRateMeter(True)
 
         # myTexture = self.loader.loadTexture("test.jpg")
         # myTexture.set_format(Texture.F_rgba32)
@@ -75,11 +74,16 @@ class FogDemo(ShowBase):
         self.taskMgr.add(self.updateColors, "update colors")
         self.colors = PTAVecBase3f([Vec3F(1, 0.8, 0), Vec3F(1, 0, 0)])
         self.dummy.set_shader_input("kage", self.colors)
+        self.mousePos = PTAVecBase2f([0, 0])
+
+        self.colors = PTAVecBase3f([Vec3F(1, 0.8, 0), Vec3F(1, 0, 0)])
+        self.dummy.set_shader_input("kage", self.colors)
 
         self.outputVertexes = Texture("buffer")
         self.outputVertexes.setupBufferTexture(512, Texture.T_float, Texture.F_rgba32, GeomEnums.UH_static)
 
         self.dummy.set_shader_input("someimg", self.outputVertexes)
+        self.dummy.set_shader_input("mouse", self.mousePos)
         self.dummy.set_shader_input("fromTex", tex)
         self.dummy.set_shader_input("toTex", tex2)
 
@@ -96,9 +100,19 @@ class FogDemo(ShowBase):
         props = WindowProperties()
         props.setTitle('Marching Cubes')
         props.setIconFilename("pandaIcon.ico")
+        #props.setFullscreen(1)
+        #props.setSize(1920, 1080)
+        self.base.openMainWindow()
         self.base.win.requestProperties(props)
+        self.base.graphicsEngine.openWindows()
+        self.base.setFrameRateMeter(True)
 
     def updateColors(self, task):
+        if self.base.mouseWatcherNode.hasMouse():
+            self.mousePos[0] = self.base.mouseWatcherNode.getMouseX()
+            self.mousePos[0] = self.base.mouseWatcherNode.getMouseY()
+
+
         switcher = task.time / 10
         self.colors.setData(PTAVecBase3f([Vec3F(1 - switcher, 0, 0), Vec3F(switcher, 0, 0)]))
 
@@ -107,16 +121,16 @@ class FogDemo(ShowBase):
         # Dispatch the compute shader, right now!
         self.base.graphicsEngine.dispatch_compute((1024, 1, 1), self.sattr, self.base.win.get_gsg())
 
-        self.base.graphicsEngine.extractTextureData(self.outputVertexes, base.win.gsg)
-        idk = self.outputVertexes.getRamImage()
-        idk = memoryview(idk).cast('f')
-
-        i = 0
-        amount = 4
-        print("start")
-        while i < len(idk):
-            print([idk[i+x] for x in range(amount)])
-            i += 4
+        # self.base.graphicsEngine.extractTextureData(self.outputVertexes, base.win.gsg)
+        # idk = self.outputVertexes.getRamImage()
+        # idk = memoryview(idk).cast('f')
+        #
+        # i = 0
+        # amount = 4
+        # print("start")
+        # while i < len(idk):
+        #     print([idk[i+x] for x in range(amount)])
+        #     i += 4
         return Task.cont
 
 demo = FogDemo()
