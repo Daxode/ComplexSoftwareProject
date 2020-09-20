9f7dc48245aba410d9528f8e1bffb66ffa2ef77d
dani485b <dani485b@gmail.com>
2020-09-20 22:54:45 +0200

    Removed redundant imports in main file

--- main.py
+++ main.py
@@ -1,39 +1,7 @@
-import math
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import Fog
-from panda3d.core import TextNode
-from direct.gui.OnscreenText import OnscreenText
-from direct.showbase.DirectObject import DirectObject
-from direct.interval.MetaInterval import Sequence
-from direct.interval.LerpInterval import LerpFunc
-from direct.interval.FunctionInterval import Func
 from panda3d.core import WindowProperties
-from direct.showbase.DirectObject import DirectObject
-from direct.gui.DirectGui import *
-from direct.interval.IntervalGlobal import *
-from panda3d.core import lookAt
-from panda3d.core import GeomVertexFormat, GeomVertexData
-from panda3d.core import Geom, GeomTriangles, GeomVertexWriter
-from panda3d.core import Texture, GeomNode
-from panda3d.core import PerspectiveLens
-from panda3d.core import CardMaker
-from panda3d.core import Light, Spotlight
-from panda3d.core import TextNode
-from panda3d.core import LPoint3, LVector3, LVecBase3f
-from panda3d.core import AmbientLight, PointLight
 from direct.task import Task
-from direct.actor.Actor import Actor
-from direct.filter.CommonFilters import CommonFilters
-from panda3d.core import PerlinNoise3
-from panda3d.core import NodePath
-from panda3d.core import Thread
-from panda3d.core import TransparencyAttrib
-from panda3d.core import Shader
-from panda3d.core import Vec3F, ShaderAttrib, ComputeNode, GraphicsPipe
 import math
-import sys
-import numpy as np
-import random
 from panda3d.core import loadPrcFile
 from BaseData import ShowBaseData
 
